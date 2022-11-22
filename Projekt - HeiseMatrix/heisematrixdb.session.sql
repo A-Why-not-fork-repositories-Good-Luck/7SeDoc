1e63fb0bd87dce3fbdf3eea6270ac94195b6a0d4
Skrap3r <lassedoerjer@gmail.com>
2022-11-22 13:29:30 +0100

    commit

--- Projekt - HeiseMatrix/heisematrixdb.session.sql
+++ Projekt - HeiseMatrix/heisematrixdb.session.sql
@@ -1,66 +1,66 @@
 -- @block
-CREATE TABLE `User`(
+CREATE TABLE `user`(
     `user_id` VARCHAR(255) NOT NULL PRIMARY KEY,
     `username` VARCHAR(255) NOT NULL 
 );
-CREATE TABLE `Workshop`(
+CREATE TABLE `workshop`(
     `workshop_id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `thema` INT UNSIGNED NULL,
     `name` VARCHAR(255) NULL
 );
-CREATE TABLE `Participation_disc`(
+CREATE TABLE `participation_disc`(
     `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `user` VARCHAR(255) NOT NULL,
     `discussion` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `Discussion`(
+CREATE TABLE `discussion`(
     `discussion_id` VARCHAR(255) NOT NULL PRIMARY KEY,
     `thema` INT UNSIGNED NULL,
     `question` VARCHAR(255) NULL,
     `creator` VARCHAR(255) NOT NULL
 );
-CREATE TABLE `Thema`(
+CREATE TABLE `thema`(
     `thema_id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    `Thema` ADD UNIQUE `thema_name_unique`(`name`);
-CREATE TABLE `Participation_work`(
+    `thema` ADD UNIQUE `thema_name_unique`(`name`);
+CREATE TABLE `participation_work`(
     `id` INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `user` VARCHAR(255) NOT NULL,
     `workshop` INT UNSIGNED NOT NULL
 );
 
 ALTER TABLE
-    `Participation_work` ADD CONSTRAINT `participation_work_user_foreign` FOREIGN KEY(`user`) REFERENCES `User`(`user_id`);
+    `participation_work` ADD CONSTRAINT `participation_work_user_foreign` FOREIGN KEY(`user`) REFERENCES `user`(`user_id`);
 ALTER TABLE
-    `Participation_work` ADD CONSTRAINT `participation_work_workshop_foreign` FOREIGN KEY(`workshop`) REFERENCES `Workshop`(`workshop_id`);
+    `participation_work` ADD CONSTRAINT `participation_work_workshop_foreign` FOREIGN KEY(`workshop`) REFERENCES `workshop`(`workshop_id`);
 ALTER TABLE
-    `Workshop` ADD CONSTRAINT `workshop_thema_foreign` FOREIGN KEY(`thema`) REFERENCES `Thema`(`thema_id`);
+    `workshop` ADD CONSTRAINT `workshop_thema_foreign` FOREIGN KEY(`thema`) REFERENCES `thema`(`thema_id`);
 ALTER TABLE
-    `Participation_disc` ADD CONSTRAINT `participation_disc_user_foreign` FOREIGN KEY(`user`) REFERENCES `User`(`user_id`);
+    `participation_disc` ADD CONSTRAINT `participation_disc_user_foreign` FOREIGN KEY(`user`) REFERENCES `user`(`user_id`);
 ALTER TABLE
-    `Participation_disc` ADD CONSTRAINT `participation_disc_discussion_foreign` FOREIGN KEY(`discussion`) REFERENCES `Discussion`(`discussion_id`);
+    `participation_disc` ADD CONSTRAINT `participation_disc_discussion_foreign` FOREIGN KEY(`discussion`) REFERENCES `discussion`(`discussion_id`);
 ALTER TABLE
-    `Discussion` ADD CONSTRAINT `discussion_thema_foreign` FOREIGN KEY(`thema`) REFERENCES `Thema`(`thema_id`);
+    `discussion` ADD CONSTRAINT `discussion_thema_foreign` FOREIGN KEY(`thema`) REFERENCES `thema`(`thema_id`);
 ALTER TABLE
-    `Discussion` ADD CONSTRAINT `discussion_creator_foreign` FOREIGN KEY(`creator`) REFERENCES `User`(`user_id`);
+    `discussion` ADD CONSTRAINT `discussion_creator_foreign` FOREIGN KEY(`creator`) REFERENCES `user`(`user_id`);
 
 
 -- @block
-DROP TABLE Participation_disc;
-DROP TABLE Participation_work;
-DROP TABLE Discussion;
-DROP TABLE Workshop;
-DROP TABLE Thema;
+DROP TABLE participation_disc;
+DROP TABLE participation_work;
+DROP TABLE discussion;
+DROP TABLE workshop;
+DROP TABLE thema;
 DROP TABLE user;
 
 -- @block
 SELECT * from user;
-SELECT * from Thema;
-SELECT * from Workshop;
-SELECT * from Discussion;
-SELECT * from Participation_disc;
-SELECT * from Participation_work;
+SELECT * from thema;
+SELECT * from workshop;
+SELECT * from discussion;
+SELECT * from participation_disc;
+SELECT * from participation_work;
 
  
